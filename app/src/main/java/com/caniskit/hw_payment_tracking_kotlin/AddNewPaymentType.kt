8acb5624091947a90989a2a3be481e2b2fd62041
Osman Can iskit <caniskit@gmail.com>
2022-09-18 12:51:19 +0300

    initial commit kotlin project

---
+++ app/src/main/java/com/caniskit/hw_payment_tracking_kotlin/AddNewPaymentType.kt
@@ -0,0 +1,11 @@
+package com.caniskit.hw_payment_tracking_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNewPaymentType : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_payment_type)
+    }
+}
